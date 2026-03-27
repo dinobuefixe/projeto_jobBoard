11b33b3f38909514e04ea6289ad22efdb53220cb
RonaldoGoatSii <172612901+RonaldoGoatSii@users.noreply.github.com>
2026-03-27 21:57:00 +0000

    feat: updating views.py and models.py in searh and jobListings

--- jobListings/views.py
+++ jobListings/views.py
@@ -4,13 +4,12 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from django.shortcuts import render
-from .models import JobListing
+
+from .models import JobListing  
 
 def index(request):
     job_list = JobListing.objects.all()
     return render(request, "jobListings/index.html", {"job_list": job_list})
-
 
 def job_detail(request, pk):
     job = get_object_or_404(JobListing, pk=pk)
--- search/views.py
+++ search/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from .models import Job
+from .models import JobListing
 from django.db.models import Q 
+from django.utils import timezone
 
 def search_view(request):
-    results = Job.objects.all()
+    from django.shortcuts import render
+    from django.db.models import Q
+    from jobListings.models import JobListing  
+    from django.utils import timezone
+
+def search_view(request):
+    results = JobListing.objects.all()
 
     keyword = request.GET.get('keyword')
     stack = request.GET.get('stack')
@@ -11,12 +18,18 @@ def search_view(request):
     seniority = request.GET.get('seniority')
 
     if keyword:
-        results = results.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword))
+        results = results.filter(
+            Q(title__icontains=keyword) |
+            Q(content__icontains=keyword) 
+        )
+
     if stack:
         results = results.filter(stack__icontains=stack)
+
     if location:
         results = results.filter(location__icontains=location)
+
     if seniority:
-        results = results.filter(seniority__iexact=seniority)
+        results = results.filter(level__iexact=seniority)  
 
     return render(request, 'search/search.html', {'results': results})
